3a87628fc1509c3cbfe25abce145b2ac211801f8
gmmns <gemma.ns.16@gmail.com>
2016-11-27 23:20:53 +0100

    Inici del projecte

---
+++ MenMed/app/src/main/java/edu/upc/pma/gemmanavarro/menmed/menu.java
@@ -0,0 +1,13 @@
+package edu.upc.pma.gemmanavarro.menmed;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class menu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+    }
+}
